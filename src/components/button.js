efe67b183afe5abf0a58cbf00a71709390897850
ndjndj <teq.ndj@gmail.com>
2021-01-03 17:01:20 +0900

    implement button.js

---
+++ src/components/button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/button.css';
+import { Link } from 'gatsby';
+
+export default function Button(props) {
+    const link = props.link;
+    const sentence = props.sentence;
+    return (
+        <Link to={link}>
+            {sentence}
+        </Link>
+    );
+}

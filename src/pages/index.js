c9e12a2a414f0543f647b3be0264a3ad8ea2cd47
ndjndj <teq.ndj@gmail.com>
2021-01-03 14:48:05 +0900

    inplement header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React from 'react';
+import Header from '../components/header';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Header />
 }

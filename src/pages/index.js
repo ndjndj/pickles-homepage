be28e697d9a34dda688e427c10b5d240e5ea4d4d
ndjndj <teq.ndj@gmail.com>
2021-01-03 16:57:33 +0900

    aboutPickles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/header';
 import Wrapper from '../components/wrapper';
+import AboutPickles from '../components/about-pickles';
 
 export default function Home() {
   return (
     <React.Fragment>
       <Header />
-      <Wrapper WrapperId={"about-pickles"} title={"About Pickles"} child={<div>div</div>}/>
+      <Wrapper WrapperId={"about-pickles"} title={"About Pickles"} child={<AboutPickles />}/>
     </React.Fragment>
   );
 }

9f1869ba925b4fb70e1b8c60327af9d92c576862
ndjndj <teq.ndj@gmail.com>
2021-01-05 01:15:04 +0900

    change name

--- src/components/flex-wrapper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../styles/flex-wrapper.css';
-import Wrapper from '../components/wrapper';
-
-export default function FlexWrapper() {
-    
-}
---
+++ src/components/two-columns.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/two-columns.css';
+import Wrapper from '../components/wrapper';
+
+export default function TwoColumns() {
+    return (
+
+    );
+}

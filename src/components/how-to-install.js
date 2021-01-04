c2e2d8e226f291d302ae7d56d809c44e0b5b154b
ndjndj <teq.ndj@gmail.com>
2021-01-04 12:56:40 +0900

    add how-to-install

---
+++ src/components/how-to-install.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/how-to-install.css';
+import Wrapper from '../components/wrapper';
+
+export default function ReleaseNote() {
+    return (
+        <Wrapper wrapperId="release-note" title="Release Note">
+            <div class="scroll-box">
+                <dl>
+                    <dt>December 28th, 2020</dt>
+                    <dd>Created this page.</dd>
+                    <dt>December 21th, 2020</dt>
+                    <dd>Released Pickles ver 1.0</dd>
+                </dl>
+            </div>
+        </Wrapper>
+    );
+}

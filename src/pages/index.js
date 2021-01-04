91488992ce25b02daa5fd8f9580cf6905f9ba997
ndjndj <teq.ndj@gmail.com>
2021-01-04 23:41:27 +0900

    implement how-to-use.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
         <ReleaseNote />
         <HowToInstall />
         <HowToUnInstall />
+        
         <PrivacyAssurance />
         <Disclaimer />
       </div>

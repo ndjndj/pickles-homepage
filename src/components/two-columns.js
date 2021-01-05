1ee689f45b32d6fa4317c7125238399b1144ab9b
ndjndj <teq.ndj@gmail.com>
2021-01-05 23:26:38 +0900

    add img-wrapper.js

--- src/components/two-columns.js
+++ src/components/two-columns.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/two-columns.css';
-import Wrapper from '../components/wrapper';
 
-export default function TwoColumns() {
+export default function TwoColumns(props) {
+    const leftComponentType = props.leftComponentType;
+    const rightComponentType = props.rightComponentType;
     return (
+        <div className="two-columns">
+            <div className={leftComponentType + " left "}>
 
+
+            </div>
+        </div>
     );
 }

5d8e077078d1277670c6965117271c505eee6e3b
ndjndj <teq.ndj@gmail.com>
2021-01-08 01:31:50 +0900

    implement bullet-wrapper.js

--- src/components/bullet-wrapper.js
+++ src/components/bullet-wrapper.js
@@ -7,13 +7,13 @@ export default function BulletWrapper(props) {
     const direction = props.direction;
     const typeOfNum = props.typeOfNum;
 
-    function olul(isNum, callback){
-        return isNum ? <ol>{callback()}</ol> : <ul>{callback()}</ul>
+    function olul(isNum, bullets){
+        return isNum ? <ol>{writeBullets(bullets)}</ol> : <ul>{writeBullets(bullets)}</ul>
     }
 
     function writeBullets(bullets) {
         bullets.map(
-            
+            bullet => <li>{bullet[i]}</li>
         );
     }
 
@@ -21,11 +21,7 @@ export default function BulletWrapper(props) {
         <div className={direction + " description"}>
             <h3><span>{title}</span></h3>
 
-            {
-
-
-
-            }
+            {olul(typeOfNum, bullets)}
         </div>
     );
 }

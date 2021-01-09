2851ba0e007f054a24e56c6d8ceb5251516400d2
ndjndj <teq.ndj@gmail.com>
2021-01-09 20:23:51 +0900

    fix bullet-wrapper.js

--- src/components/bullet-wrapper.js
+++ src/components/bullet-wrapper.js
@@ -2,18 +2,25 @@ import React from 'react';
 import '../styles/bullet-wrapper.css';
 
 export default function BulletWrapper(props) {
-    const bullets = props.bullets;
+    //const bullets = props.bullets;
     const title = props.title;
     const direction = props.direction;
     const isNum = props.typeOfNum;
+    let bullets = [];
+
+    for (var i=0; i<props.bullets.length; i++) {
+        bullets.push(<li>{props.bullets[i]}</li>);
+    }
+    console.log(bullets);
 
     function olul(isNum, bullets){
-        return isNum ? <ol>{writeBullets(bullets)}</ol> : <ul>{writeBullets(bullets)}</ul>
+        return isNum ? <ol>{bullets}</ol> : <ul>{bullets}</ul>
     }
 
     function writeBullets(bullets) {
+        console.log(bullets)
         bullets.map(
-            bullet => <li>{bullet}</li>
+            (bullet) => {return <li>{bullet}</li>;}
         );
     }
 

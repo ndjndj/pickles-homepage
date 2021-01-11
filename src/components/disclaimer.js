a1d820aec3031f3b46aae43079f60913fb555b44
ndjndj <teq.ndj@gmail.com>
2021-01-11 20:10:21 +0900

    fix disclaimer.js

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../styles/disclaimer.css';
 import Wrapper from '../components/wrapper';
+import Sentence from '../components/sentence';
 
 export default function Disclaimer() {
+    const sentences = [
+        "Please note that we cannot be held responsible for any damage caused by the use of this extension.",
+        "Use of Pickles must be done at your own risk."
+    ];
     return (
         <Wrapper wrapperId="disclaimer" title="Disclaimer">
-            <div class="sentence">
-                <p>Please note that we cannot be held responsible for any damage caused by the use of this extension.</p>
-                <p>Use of Pickles must be done at your own risk.</p>
-            </div>
+            <Sentence sentences={sentences}/>
         </Wrapper>
     );
 }

5b68106eeebc9de985adfb3fd6d78d1f34ba34f0
ndjndj <teq.ndj@gmail.com>
2021-01-12 04:50:19 +0900

    fix release-note.js

--- src/components/release-note.js
+++ src/components/release-note.js
@@ -3,14 +3,37 @@ import '../styles/release-note.css';
 import Wrapper from '../components/wrapper';
 
 export default function ReleaseNote() {
+    const releaseInfomations = [
+        {
+            'dt': 'December 28th, 2020',
+            'dd': 'Created this page.'
+        },
+        {
+            'dt': 'December 21th, 2020',
+            'dd': 'Released Pickles ver 1.0'
+        }
+    ];
+
+    function createReleaseNotes(releaseInfomations) {
+        let jsx = [];
+
+        for (var i=0; i<releaseInfomations.length; i++) {
+            jsx.push(
+                <React.Fragment key={i}>
+                    <dt>{releaseInfomations[i]['dt']}</dt>
+                    <dd>{releaseInfomations[i]['dd']}</dd>
+                </React.Fragment>
+            );
+        }
+        
+        return jsx;
+    }
+
     return (
         <Wrapper wrapperId="release-note" title="Release Note">
             <div className="scroll-box">
                 <dl>
-                    <dt>December 28th, 2020</dt>
-                    <dd>Created this page.</dd>
-                    <dt>December 21th, 2020</dt>
-                    <dd>Released Pickles ver 1.0</dd>
+                    {createReleaseNotes(releaseInfomations)}
                 </dl>
             </div>
         </Wrapper>

28d3e709877e9649bd6a6dfa4fec6d02ed6774ba
ndjndj <teq.ndj@gmail.com>
2021-01-12 05:04:41 +0900

    fix button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../styles/button.css';
-import { Link } from 'gatsby';
 
 export default function Button(props) {
     const link = props.link;
     const sentence = props.sentence;
     return (
-        <Link to={link} className="btn" target="_blank" rel="noopener noreferrer">
+        <a href={link} className="btn" target="_blank" rel="noopener noreferrer">
             {sentence}
-        </Link>
+        </a>
     );
 }

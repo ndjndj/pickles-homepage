ae8f4c2c45dc61add03603f13b13211b73674c38
ndjndj <teq.ndj@gmail.com>
2021-01-12 03:51:49 +0900

    fix page-index.js

--- src/components/page-index.js
+++ src/components/page-index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import '../styles/page-index.css';
 import Wrapper from '../components/wrapper';
+import { Link } from 'gatsby';
 
 export default function PageIndex() {
+    const links = {
+        '#about-pickles': 'About Pickles',
+        '#release-note': 'Release note',
+        '#how-to-install': 'How to install',
+        '#how-to-uninstall': 'How to uninstall',
+        '#how-to-use': 'How to use',
+        '#privacy-assurance': 'Privacy assurance',
+        '#disclaimer': 'Disclaimer'
+    };
+    
     return (
         <Wrapper wrapperId="page-index" title="Index">
             <ol>
-                <li><a href="#about-pickles">About Pickles</a></li>
-                <li><a href="#release-note">Release note</a></li>
-                <li><a href="#how-to-install">How to install</a></li>
-                <li><a href="#how-to-uninstall">How to uninstall</a></li>
-                <li><a href="#how-to-use">How to use</a></li>
-                <li><a href="#privacy-assurance">Privacy assurance</a></li>
-                <li><a href="#disclaimer">Disclaimer</a></li>
+                {
+                    links.map((link) => {})
+                }
             </ol>
         </Wrapper>
     );

6334cd290f4d81c6a1cca5881792495c8c0ea150
ndjndj <teq.ndj@gmail.com>
2021-01-12 05:23:26 +0900

    fix how-to-uninstall.js

--- src/components/how-to-uninstall.js
+++ src/components/how-to-uninstall.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import '../styles/how-to-uninstall.css';
 import Wrapper from '../components/wrapper';
+import BulletWrapper from '../components/bullet-wrapper';
 import LP_2 from '../img/PicklesLP_2.png';
 
 export default function HowToUnInstall() {
+    const bullets = [
+        'Click to Pickles icon.',
+        'Click to tabs button.',
+        'Click to Pickle to open tabs.',
+        'Click to INFO button to check the Pickle.',
+        'Click to DEL button to delete the Pickle.'
+    ];
+    const title = 'How to Pickle.';
+    const direction = 'left';
+    const isNum = true;
+    
     return (
         <Wrapper wrapperId="how-to-uninstall" title="How to uninstall">
             <div className="flex-wrapper">
-                <div className="left description">
-                    <div>
-                        <h3><span>How to Pickle.</span></h3>
-                            <ol>
-                                <li>Click to Pickles icon.</li>
-                                <li>Click to tabs button.</li>
-                                <li>Click to Pickle to open tabs.</li>
-                                <li>Click to INFO button to check the Pickle.</li>
-                                <li>Click to DEL button to delete the Pickle.</li>
-                            </ol>
-                    </div>
-                </div>
+                <BulletWrapper
+                    bullets = {bullets}
+                    title = {title}
+                    direction = {direction}
+                    isNum = {isNum}
+                />
 
                 <div className="right img">
                     <img src={LP_2} alt="" />
